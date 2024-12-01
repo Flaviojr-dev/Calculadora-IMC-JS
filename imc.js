eb64fa25a325af5765db70131f5d3641605fdbe9
Flaviojr-dev <flaviobarbosabsjt@gmail.com>
2024-11-30 23:43:27 -0300

    Finalizado

--- imc.js
+++ imc.js
@@ -13,11 +13,11 @@ function imc () {
         let classificacao = "";
 
         if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.'
+            classificacao = 'abaixo do peso // Eu te amo, sogrinha! '
         }else if (valorIMC < 25){
-            classificacao = 'peso ideal. Parabéns!'
+            classificacao = 'peso ideal // Eu te amo, sogrinha! '
         }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
+            classificacao = 'levemente acima do peso // Eu te amo, sogrinha!';
         }else if (valorIMC < 35){
             classificacao = 'com obesidade grau I.'
         }else if (valorIMC < 40){
